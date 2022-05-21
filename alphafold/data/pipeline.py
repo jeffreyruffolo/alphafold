24a5281ca939835ec6cc872c715f0c96e589e2ea
Jeff <jeffreyruffolo@gmail.com>
2022-05-21 14:01:16 -0500

    rearrange pipeline

--- alphafold/data/pipeline.py
+++ alphafold/data/pipeline.py
@@ -175,11 +175,39 @@ class DataPipeline:
         jackhmmer_uniref90_result = run_msa_tool(
             self.jackhmmer_uniref90_runner, input_fasta_path,
             uniref90_out_path, 'sto', self.use_precomputed_msas)
-        mgnify_out_path = os.path.join(msa_output_dir, 'mgnify_hits.sto')
-        jackhmmer_mgnify_result = run_msa_tool(self.jackhmmer_mgnify_runner,
-                                               input_fasta_path,
-                                               mgnify_out_path, 'sto',
-                                               self.use_precomputed_msas)
+        if not self.use_single_sequence:
+            mgnify_out_path = os.path.join(msa_output_dir, 'mgnify_hits.sto')
+            jackhmmer_mgnify_result = run_msa_tool(
+                self.jackhmmer_mgnify_runner, input_fasta_path,
+                mgnify_out_path, 'sto', self.use_precomputed_msas)
+
+            uniref90_msa = parsers.parse_stockholm(
+                jackhmmer_uniref90_result['sto'])
+            uniref90_msa = uniref90_msa.truncate(max_seqs=self.uniref_max_hits)
+            mgnify_msa = parsers.parse_stockholm(
+                jackhmmer_mgnify_result['sto'])
+            mgnify_msa = mgnify_msa.truncate(max_seqs=self.mgnify_max_hits)
+
+            if self._use_small_bfd:
+                bfd_out_path = os.path.join(msa_output_dir,
+                                            'small_bfd_hits.sto')
+                jackhmmer_small_bfd_result = run_msa_tool(
+                    self.jackhmmer_small_bfd_runner, input_fasta_path,
+                    bfd_out_path, 'sto', self.use_precomputed_msas)
+                bfd_msa = parsers.parse_stockholm(
+                    jackhmmer_small_bfd_result['sto'])
+            else:
+                bfd_out_path = os.path.join(msa_output_dir,
+                                            'bfd_uniclust_hits.a3m')
+                hhblits_bfd_uniclust_result = run_msa_tool(
+                    self.hhblits_bfd_uniclust_runner, input_fasta_path,
+                    bfd_out_path, 'a3m', self.use_precomputed_msas)
+                bfd_msa = parsers.parse_a3m(hhblits_bfd_uniclust_result['a3m'])
+
+            msa_features = make_msa_features(
+                (uniref90_msa, bfd_msa, mgnify_msa))
+        else:
+            msa_features = FeatureDict()
 
         msa_for_templates = jackhmmer_uniref90_result['sto']
         msa_for_templates = parsers.truncate_stockholm_msa(
@@ -206,46 +234,16 @@ class DataPipeline:
         with open(pdb_hits_out_path, 'w') as f:
             f.write(pdb_templates_result)
 
-        if not self.use_single_sequence:
-            uniref90_msa = parsers.parse_stockholm(
-                jackhmmer_uniref90_result['sto'])
-            uniref90_msa = uniref90_msa.truncate(max_seqs=self.uniref_max_hits)
-            mgnify_msa = parsers.parse_stockholm(
-                jackhmmer_mgnify_result['sto'])
-            mgnify_msa = mgnify_msa.truncate(max_seqs=self.mgnify_max_hits)
+        pdb_template_hits = self.template_searcher.get_template_hits(
+            output_string=pdb_templates_result, input_sequence=input_sequence)
 
-            pdb_template_hits = self.template_searcher.get_template_hits(
-                output_string=pdb_templates_result,
-                input_sequence=input_sequence)
+        templates_result = self.template_featurizer.get_templates(
+            query_sequence=input_sequence, hits=pdb_template_hits)
 
-            if self._use_small_bfd:
-                bfd_out_path = os.path.join(msa_output_dir,
-                                            'small_bfd_hits.sto')
-                jackhmmer_small_bfd_result = run_msa_tool(
-                    self.jackhmmer_small_bfd_runner, input_fasta_path,
-                    bfd_out_path, 'sto', self.use_precomputed_msas)
-                bfd_msa = parsers.parse_stockholm(
-                    jackhmmer_small_bfd_result['sto'])
-            else:
-                bfd_out_path = os.path.join(msa_output_dir,
-                                            'bfd_uniclust_hits.a3m')
-                hhblits_bfd_uniclust_result = run_msa_tool(
-                    self.hhblits_bfd_uniclust_runner, input_fasta_path,
-                    bfd_out_path, 'a3m', self.use_precomputed_msas)
-                bfd_msa = parsers.parse_a3m(hhblits_bfd_uniclust_result['a3m'])
-
-            templates_result = self.template_featurizer.get_templates(
-                query_sequence=input_sequence, hits=pdb_template_hits)
-
-            sequence_features = make_sequence_features(
-                sequence=input_sequence,
-                description=input_description,
-                num_res=num_res)
-
-            msa_features = make_msa_features(
-                (uniref90_msa, bfd_msa, mgnify_msa))
-        else:
-            msa_features = FeatureDict()
+        sequence_features = make_sequence_features(
+            sequence=input_sequence,
+            description=input_description,
+            num_res=num_res)
 
         logging.info('Uniref90 MSA size: %d sequences.', len(uniref90_msa))
         logging.info('BFD MSA size: %d sequences.', len(bfd_msa))
